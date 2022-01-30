8c3f80a27c3bd16d6b927cf9c5c5b4c10cf492dc
Ivana Zeljic <iz46273@oss.unist.hr>
2022-01-30 20:54:24 +0100

    updated

--- Booking/src/main/java/com/lolekibolek/Booking/persistence/api/UserController.java
+++ Booking/src/main/java/com/lolekibolek/Booking/persistence/api/UserController.java
@@ -58,8 +58,12 @@ public class UserController {
 			
 			for (int i = 0; i < allReservations.size(); i++) {
 				if (allReservations.get(i).getCheckInDate().isAfter(today) || allReservations.get(i).getCheckInDate().equals(today)) {
-					if (allReservations.get(i).getBooked().equals(true))
+					if (allReservations.get(i).getBooked().equals(true)) {
+						System.out.println("Check-in: " + allReservations.get(i).getCheckInDate());
+						System.out.println("Today " + LocalDate.now());
 						futureReservations.add(allReservations.get(i));
+					}
+						
 				}
 			}
 			
@@ -69,7 +73,7 @@ public class UserController {
 						pastReservations.add(allReservations.get(i));
 				}
 			}
-			
+						
 			Boolean hasPast = true;
 			Boolean hasFuture = true;
 			if (futureReservations.isEmpty())
@@ -78,8 +82,8 @@ public class UserController {
 				hasPast = false;
 			model.addAttribute("hasFuture", hasFuture);
 			model.addAttribute("hasPast", hasPast);
-			model.addAttribute(pastReservations);
-			model.addAttribute(futureReservations);
+			model.addAttribute("pastReservations", pastReservations);
+			model.addAttribute("futureReservations", futureReservations);
 			
 			return "profileUser";
 		}
